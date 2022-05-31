ef2a7e22bbf0b5576eb7fac0f08b312f1abf1b2b
Tyler Thames <tylerthames@bellsouth.net>
2022-05-30 19:29:58 -0500

    Added constraints

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        messageLabel.text = ""
 
     }
 
