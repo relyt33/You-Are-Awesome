a027b614e71b645d494636fe4c101c3df5e9a8b1
Tyler Thames <tylerthames@bellsouth.net>
2022-06-02 18:30:03 -0500

    2.7 if else statement

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet var messageLabel: UILabel!
     
     @IBOutlet var imageView: UIImageView!
@@ -19,8 +18,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func messageButton(_ sender: UIButton) {
-        messageLabel.text = "You are awesome!"
-        imageView.image = UIImage(named: "image0")
+        let awesomeMessage = "You are awesome!"
+        let greatMessage = "You are great!"
+        let bombMessage = "You are da bomb!"
+        
+        if messageLabel.text == awesomeMessage{
+            messageLabel.text = greatMessage
+            imageView.image = UIImage(named: "image1")
+        } else if messageLabel.text == greatMessage {
+            messageLabel.text = bombMessage
+            imageView.image = UIImage(named: "image2")
+        } else {
+            messageLabel.text = awesomeMessage
+            imageView.image = UIImage(named: "image0")
+        }
     }
     
 }
